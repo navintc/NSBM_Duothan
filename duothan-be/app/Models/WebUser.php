b6ea663ebf45ff2b765c2fa8fc7f418fafa3ae94
navintc <navin.thamindu@gmail.com>
2023-02-24 15:19:17 +0530

    Adding the login api

--- duothan-be/app/Models/WebUser.php
+++ duothan-be/app/Models/WebUser.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class WebUser extends Model
+class WebUser extends Authenticatable
 {
     use HasFactory;
+
+    protected $fillable = [
+        '',''
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 }
--- duothan-be/database/seeders/WebUserSeeder.php
+++ duothan-be/database/seeders/WebUserSeeder.php
@@ -4,8 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+//hash is used for the password
+use Illuminate\Support\Facades\Hash;
 
-class WebUserSeeder extends Seeder
+class WebUserseeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,6 +17,44 @@ class WebUserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('web_users')->insert([
+            'name' => "Bileka Karunarathne",
+            'email' => "bileka@bileka.com",
+            'password' => Hash::make('password'),
+            'phone' => "0757555359",
+            'userType' => 0,
+        ]);
+
+        DB::table('web_users')->insert([
+            'name' => "Ruby Windowsinghe",
+            'email' => "ruby@bileka.com",
+            'password' => Hash::make('password'),
+            'phone' => "07545754533",
+            'userType' => 1,
+        ]);
+
+        DB::table('web_users')->insert([
+            'name' => "Digitalia Javarathne",
+            'email' => "talia@bileka.com",
+            'password' => Hash::make('password'),
+            'phone' => "0789574466",
+            'userType' => 1,
+        ]);
+
+        DB::table('web_users')->insert([
+            'name' => "Jenkins Kontrolean",
+            'email' => "jenkins@bileka.com",
+            'password' => Hash::make('password'),
+            'phone' => "07578644679",
+            'userType' => 1,
+        ]);
+
+        DB::table('web_users')->insert([
+            'name' => "Applein MacTavish",
+            'email' => "applein@bileka.com",
+            'password' => Hash::make('password'),
+            'phone' => "0775373759",
+            'userType' => 1,
+        ]);
     }
 }
